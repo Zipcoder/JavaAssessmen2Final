d82422e7bcbe4c8aea5db581ee24a019d9d36eeb
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-05 14:50:55 -0500

    a

---
+++ src/main/java/com/zipcodewilmington/assessment2/part6/LoveLetter.java
@@ -0,0 +1,16 @@
+package com.zipcodewilmington.assessment2.part6;
+
+public class LoveLetter {
+
+    //private String[] input = {};
+
+    public LoveLetter(){
+
+    }
+
+    public Integer[] mystery(String[] input) {
+        Integer[] answerArray = new Integer[input.length];
+
+        return answerArray;
+    }
+}
