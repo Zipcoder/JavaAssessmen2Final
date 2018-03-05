1ecfa95339d61341afdf35c806a6a557b81681da
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-03-05 09:12:41 -0500

    before changes were pulled to part 1

---
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -0,0 +1,15 @@
+package java.com.zipcodewilmington.assessment2.part1;
+
+public class WuTangConcatenator {
+
+    public WuTangConcatenator() {
+
+    }
+    public Integer isWu(Integer input){
+        if (input % 3 == )
+
+
+        return input;
+    }
+
+}
