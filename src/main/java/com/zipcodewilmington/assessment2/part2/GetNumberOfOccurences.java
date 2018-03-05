4e054cc9d90a03044ac34a43896c970a00acbb65
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-03-05 09:57:31 -0500

    Tariq instructed to skip

--- src/main/java/com/zipcodewilmington/assessment2/part2/GetNumberOfOccurences.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/GetNumberOfOccurences.java
@@ -3,4 +3,25 @@ package com.zipcodewilmington.assessment2.part2;
 public class GetNumberOfOccurences {
 
 
+    Object[] inputArray;
+    Integer value;
+
+    public GetNumberOfOccurences(Object[] inputArray, Object count) {
+
+    }
+
+
+
+    public static Object getNumberOfOccurrences(Object[] inputArray, Object object) {
+
+        int value = 0;
+        for (int x = 0; x < inputArray.length; x++) {
+            if (inputArray[x].equals(object)) {
+                value++;
+            }
+        }
+
+        return value;
+    }
+
 }
