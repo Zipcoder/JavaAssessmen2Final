a427a9c264e886658a02ba5ff3e667b43ec13042
Karousha Fennimore <karousha@gmail.com>
2018-03-05 09:44:39 -0500

    saving

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,23 +1,34 @@
 package com.zipcodewilmington.assessment2.part2;
 
 public class ArrayUtility<T> {
-    
-    public ArrayUtility(Integer[] inputArray) {
+
+
+    private T inputArray;
+    private T arrayToMerge;
+    private T valueToEvaluate;
+    private T valueToRemove;
+
+    public ArrayUtility(T inputArray) {
+        this.inputArray = inputArray;
     }
 
-    public T getMostCommonFromMerge(Integer[] arrayToMerge, T expected) {
+    public T getMostCommonFromMerge(T arrayToMerge, T expected) {
+        this.arrayToMerge = arrayToMerge;
         return expected;
     }
 
-    public T countDuplicatesInMerge(Integer[] arrayToMerge, T valueToEvaluate) {
+    public T countDuplicatesInMerge(T arrayToMerge, T valueToEvaluate) {
+        this.arrayToMerge = arrayToMerge;
         return valueToEvaluate;
     }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        this.valueToEvaluate = valueToEvaluate;
         return null;
     }
 
     public T removeValue(T valueToRemove) {
+        this.valueToRemove = valueToRemove;
         return null;
     }
 }
