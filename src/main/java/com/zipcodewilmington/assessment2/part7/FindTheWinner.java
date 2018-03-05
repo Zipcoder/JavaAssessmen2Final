c0ab0d971ad4a4e70ea0dfd391fc1f8e536fbf55
ghost1497 <rfrankie04@gmail.com>
2018-03-05 13:16:43 -0500

    got it. was confused a little but i remembered... RTFM

--- src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
+++ src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
@@ -5,7 +5,19 @@ import java.util.ArrayList;
 public class FindTheWinner {
 
     public String winner(Integer[] leon, Integer[] wilhelm, String input) {
-        ArrayList<Integer> leonScore = new ArrayList<>();
-        ArrayList
+        Integer leonScore = 0;
+        Integer wilhelmScore = 0;
+
+        for (int i = (input.equals("Odd") ? 1 : 0); i < leon.length ; i += 2){
+            leonScore += calculateScore(leon[i], wilhelm[i]);
+            wilhelmScore += calculateScore(wilhelm[i], leon[i]);
+        }
+        return (wilhelmScore > leonScore ? "Wilhelm" : "Leon");
+
+    }
+
+    public Integer calculateScore(Integer userCard, Integer opponentCard){
+        Integer returnValue = userCard - opponentCard;
+        return returnValue;
     }
 }
