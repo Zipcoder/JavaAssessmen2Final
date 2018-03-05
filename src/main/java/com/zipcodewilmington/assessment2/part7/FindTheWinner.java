e652528f746a02dd784df86efb23deee63119f53
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-05 16:12:46 -0500

    winner if statement

--- src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
+++ src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
@@ -9,6 +9,16 @@ public class FindTheWinner {
     }
     public String winner(Integer[] player1, Integer[] player2, String input) {
 
+        if (input.equalsIgnoreCase("Even")){
+            // If "Even" is called, the player's top cards are flipped so they can see the face value.
+
+        } else if (input.equalsIgnoreCase("Odd")){
+            //If instead "Odd" is called, the top card is removed from each deck and discarded, then each flips her next card.
+
+        } else {
+            winner = "ERROR: We're sorry- you entered an invalid entry type. Please try again.";
+        }
+
         return winner;
 
     }
