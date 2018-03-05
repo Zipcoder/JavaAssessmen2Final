51be03d2a1ed10b8fd38ac7871fc32591538b435
Karousha Fennimore <karousha@gmail.com>
2018-03-05 09:32:55 -0500

    switching to make pull in part1

---
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -0,0 +1,23 @@
+package com.zipcodewilmington.assessment2.part2;
+
+public class ArrayUtility<T> {
+    
+    public ArrayUtility(Integer[] inputArray) {
+    }
+
+    public T getMostCommonFromMerge(Integer[] arrayToMerge, T expected) {
+        return expected;
+    }
+
+    public T countDuplicatesInMerge(Integer[] arrayToMerge, T valueToEvaluate) {
+        return valueToEvaluate;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        return null;
+    }
+
+    public T removeValue(T valueToRemove) {
+        return null;
+    }
+}
