4edfdb0651de25a96d3af06772be10da57e4a25b
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-03-05 13:31:20 -0500

    updated

---
+++ src/main/java/com/zipcodewilmington/assessment2/part6/LoveLetter.java
@@ -0,0 +1,25 @@
+package com.zipcodewilmington.assessment2.part6;
+
+public class LoveLetter {
+
+    public  LoveLetter(){
+
+    }
+
+    public int countToPalindrom(String str){
+        int result;
+        StringBuilder builder = new StringBuilder(str);
+        String reverseString = builder.reverse().toString();
+        if(str==reverseString){
+            result =0;
+        }else{
+            for(int i=0;i<str.length();i++){
+                if(str.charAt(i)==)
+            }
+
+
+        }
+        return 0;
+    }
+
+}
