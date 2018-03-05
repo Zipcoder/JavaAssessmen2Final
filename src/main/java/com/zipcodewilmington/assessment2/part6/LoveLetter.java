6c91f9d0f79b9ff7d0d71d34d902a4a5ff78582d
Karousha Fennimore <karousha@gmail.com>
2018-03-05 14:47:54 -0500

    almost there

--- src/main/java/com/zipcodewilmington/assessment2/part6/LoveLetter.java
+++ src/main/java/com/zipcodewilmington/assessment2/part6/LoveLetter.java
@@ -2,32 +2,26 @@ package com.zipcodewilmington.assessment2.part6;
 
 public class LoveLetter {
 
-    int count = 0;
 
-    private void checkPalindrome(String input, int left, int right) {
-        while(left >= 0 && right < input.length() && input.charAt(left) == input.charAt(right)){
-            //Check for the palindrome string if == fails then its not a palindrome!
-            count++;    //Increment the count if palindrome substring is found (to count how many palindromes are in string)
-            left--;         //Moving the left side over by one
-            right++;        //Moving the right side over by one
-        }
-    }
-    public Integer countPalindromes(String input) {
-
-        for (int i = 0; i < input.length() - 1; i++) {
-            checkPalindrome(input, i, i);           // checking if its odd length palindromic sub-string
-            checkPalindrome(input, i, i + 1);   // will never run and go into the while loop
-        }
-        return count;
-    }
 
     public Integer[] mystery(String[] input) {
         Integer[] moves = new Integer[input.length];
         for (int i = 0; i < input.length; i++) {
-            moves[i] = (countPalindromes(input[i]));
+            if(countPalindromes(input[i]) > 0) {
+                moves[i] = countPalindromes(input[i]);
+            }
         }
 
         return moves;
+    }
+
+    private int countPalindromes(String input) {
+        int counter = 0;
+        int length = input.length();
+        for (int i = 0; i < length / 2; i++) {
+            counter += Math.abs(input.toCharArray()[i] - input.toCharArray()[length - i - 1]);
+        }
+        return counter;
     }
 }
 
