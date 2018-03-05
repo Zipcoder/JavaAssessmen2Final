28517a1c653b04ca1b3a2fc267e08b7d9ff39ec0
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-03-05 10:54:29 -0500

    return

---
+++ src/main/java/com/zipcodewilmington/assessment2/part3/Evaluator.java
@@ -0,0 +1,11 @@
+package com.zipcodewilmington.assessment2.part3;
+
+import org.junit.Test;
+
+public interface Evaluator {
+
+       public void evaluateWinnerTest();
+
+
+       public void evaluateLoserTest();
+}
