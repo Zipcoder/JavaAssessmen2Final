c4299d7d0f634e75637271bda2f6bebba3065d7c
ahsonali <ahsonac@gmail.com>
2018-03-05 13:39:01 -0500

    phew, both of the unchanged test cases pass

---
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RockPaperScissors.java
@@ -0,0 +1,58 @@
+package com.zipcodewilmington.assessment2.part3;
+
+public enum RockPaperScissors {
+
+    ROCK("ROCK"),
+    PAPER("PAPER"),
+    SCISSORS("SCISSORS");
+
+    private String sign;
+
+    RockPaperScissors(String sign)
+    {
+        this.sign = sign;
+    }
+
+    public RockPaperScissors getWinningSign()
+    {
+        if (sign.equals(RockPaperScissors.ROCK))
+        {
+            return RockPaperScissors.ROCK;
+        }
+
+        else if (sign.equals(RockPaperScissors.SCISSORS))
+        {
+            return RockPaperScissors.SCISSORS;
+        }
+
+        else
+        {
+            return RockPaperScissors.PAPER;
+        }
+
+
+
+    }
+
+    public RockPaperScissors getLosingSign()
+    {
+        if (sign.equals(RockPaperScissors.SCISSORS))
+        {
+            return RockPaperScissors.ROCK;
+        }
+
+        else if (sign.equals(RockPaperScissors.ROCK))
+        {
+            return RockPaperScissors.PAPER;
+        }
+        else
+        {
+            return RockPaperScissors.SCISSORS;
+        }
+
+    }
+
+
+
+
+}
