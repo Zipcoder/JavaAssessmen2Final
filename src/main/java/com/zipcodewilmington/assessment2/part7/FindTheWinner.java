0d7b0b2c09165210f257acda363a7c7a13f366e1
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-03-05 16:21:22 -0500

    just checking

---
+++ src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
@@ -0,0 +1,21 @@
+package com.zipcodewilmington.assessment2.part7;
+
+public class FindTheWinner {
+
+    String input;
+    Integer[] array1;
+    Integer[] array2;
+
+
+
+
+
+    public String winner(Integer array1, Integer array2, String input) {
+            if (input == "Even") {
+
+            }
+       return null;
+   }
+
+
+}
