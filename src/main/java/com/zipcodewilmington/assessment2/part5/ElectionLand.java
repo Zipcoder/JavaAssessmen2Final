6d9bfe76f0f858c2b50322b7a5218948f1888217
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-05 13:53:49 -0500

    electionLand ALL tests passing htJoy

--- src/main/java/com/zipcodewilmington/assessment2/part5/ElectionLand.java
+++ src/main/java/com/zipcodewilmington/assessment2/part5/ElectionLand.java
@@ -1,8 +1,11 @@
 package com.zipcodewilmington.assessment2.part5;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ElectionLand {
 
-    private String votes = "";
+    //private String[] votes = {};
     private String personWhoGotElected = "";
 
     public ElectionLand(){
@@ -10,7 +13,72 @@ public class ElectionLand {
     }
 
 
-    public String electionWinner(String[] votes) {
+    public static Integer getNumberOfOccurrences(Object[] objectArray, Object objectToCount) {
+
+        int numOfOccurrences = 0;
+
+        for (Object element : objectArray) {
+            if (element.equals(objectToCount)) {
+                numOfOccurrences++;
+            }
+        }
+
+        return numOfOccurrences;
+    }
+
+    public static String electionWinner(String[] votes) {
+
+        /*Arrays.sort(intArray, Collections.reverseOrder());
+
+        HAHHAHAHHAHHAHHAHAHAHHAHHAHAHHAHHAHAHHAHHAHAHA I Y F B
+         */
+
+        Arrays.sort(votes, Collections.reverseOrder());
+
+        Integer theLargestSoFar = 0;
+        String personWhoGotElected = "";
+        for (int i = 0; i < votes.length; i++) {
+            Integer numberOfOccurrences = getNumberOfOccurrences(votes, votes[i]);
+            if (numberOfOccurrences > theLargestSoFar) {
+                theLargestSoFar = numberOfOccurrences;
+                personWhoGotElected = votes[i];
+            }
+        }
+        return personWhoGotElected;
+
+    }
+
+/*below are the "blank templates":
+
+    --------blank templates start:
+
+    public static Object getMostCommon(Object[] objectArray) {
+
+        Integer theLargestSoFar = 0;
+        Object mostCommonObject = null;
+        for (int i = 0; i < objectArray.length; i++) {
+            Integer numberOfOccurrences = getNumberOfOccurrences(objectArray, objectArray[i]);
+            if (numberOfOccurrences > theLargestSoFar) {
+                theLargestSoFar = numberOfOccurrences;
+                mostCommonObject = objectArray[i];
+            }
+        }
+        return mostCommonObject;
+
+    }
+
+        public String electionWinner(String[] votes) {
+        Arrays.sort(votes);
+
         return personWhoGotElected;
     }
+
+    -------blank templates end.
+
+    */
+
+    //    public Integer getNumberOfOccurrences(String[] strArray, String strToCount){
+//
+//    }
+
 }
