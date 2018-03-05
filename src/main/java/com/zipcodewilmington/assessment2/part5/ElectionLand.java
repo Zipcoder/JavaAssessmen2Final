cf822b0f8ebbac23ee497fd0ae193c5f0283541d
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-03-05 12:23:05 -0500

    All tests passing for part5

--- src/main/java/com/zipcodewilmington/assessment2/part5/ElectionLand.java
+++ src/main/java/com/zipcodewilmington/assessment2/part5/ElectionLand.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.assessment2.part5;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class ElectionLand {
 
     public ElectionLand(){
@@ -8,17 +11,28 @@ public class ElectionLand {
 
     public String electionWinner(String[] votes){
 
+
+
         String electionWinner = votes[0];
         for(int j=0;j<votes.length;j++){
             if(getNumberOfOccurences(votes,votes[j])>getNumberOfOccurences(votes,electionWinner)){
                 electionWinner=votes[j];
             }
         }
+        String[]winners =new String[1];
+        winners[0]=electionWinner;
+        int index =1;
         for(int k =0;k<votes.length;k++){
-            if(getNumberOfOccurences(votes, votes[k])==getNumberOfOccurences(votes,electionWinner)){
 
+            if(getNumberOfOccurences(votes, votes[k])==getNumberOfOccurences(votes,electionWinner)){
+                winners= Arrays.copyOf(winners,winners.length+1);
+                winners[index]=votes[k];
+                index++;
             }
         }
+        Arrays.sort(winners);
+
+        return winners[winners.length-1];
 
 
     }
