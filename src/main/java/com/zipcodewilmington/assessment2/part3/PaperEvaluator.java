9924bc58127cfe1ad07a53f1e1de1d731e84a646
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-05 11:10:13 -0500

    Part3 all tests passing

---
+++ src/main/java/com/zipcodewilmington/assessment2/part3/PaperEvaluator.java
@@ -0,0 +1,13 @@
+package com.zipcodewilmington.assessment2.part3;
+
+public class PaperEvaluator implements EvaluatorInterface {
+    @Override
+    public void evaluateWinner() {
+
+    }
+
+    @Override
+    public void evaluateLoser() {
+
+    }
+}
