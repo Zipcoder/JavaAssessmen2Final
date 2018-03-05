a5b4ad72402e99fc0e54d35c92c2e9561d4b3ddc
ghost1497 <rfrankie04@gmail.com>
2018-03-05 13:02:43 -0500

    started part 7

---
+++ src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
@@ -0,0 +1,11 @@
+package com.zipcodewilmington.assessment2.part7;
+
+import java.util.ArrayList;
+
+public class FindTheWinner {
+
+    public String winner(Integer[] leon, Integer[] wilhelm, String input) {
+        ArrayList<Integer> leonScore = new ArrayList<>();
+        ArrayList
+    }
+}
