47267865bdf8b9acedae53f14a286be9f1412554
Kthomas <kthomas@github.com>
2018-03-05 17:41:58 -0500

    update asshole

--- src/main/java/com/zipcodewilmington/assessment2/part4/Geometry.java
+++ src/main/java/com/zipcodewilmington/assessment2/part4/Geometry.java
@@ -1,4 +1,14 @@
 package com.zipcodewilmington.assessment2.part4;
 
 public class Geometry {
+    public Geometry(int height, int width, int radius) {
+    }
+
+    public int getArea(int height, int width) {
+        return 0;
+    }
+
+    public double getArea(int radius) {
+        return 0;
+    }
 }
---
+++ src/main/java/com/zipcodewilmington/assessment2/part6/LoveLetter.java
@@ -0,0 +1,7 @@
+package com.zipcodewilmington.assessment2.part6;
+
+public class LoveLetter {
+    public Integer[] mystery(String[] input) {
+        return  null;
+    }
+}
