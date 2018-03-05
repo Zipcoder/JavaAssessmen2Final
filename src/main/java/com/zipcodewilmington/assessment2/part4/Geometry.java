ae78a9ae80bdbac11c9df825823e46fd2bf18eba
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-05 11:33:47 -0500

    created and populated Geometry class

--- src/main/java/com/zipcodewilmington/assessment2/part4/Geometry.java
+++ src/main/java/com/zipcodewilmington/assessment2/part4/Geometry.java
@@ -1,4 +1,23 @@
 package com.zipcodewilmington.assessment2.part4;
 
 public class Geometry {
+    private int height;
+    private int width;
+    private int radius;
+
+    public Geometry(){
+
+    }
+
+    public Geometry(int height, int width, int radius){
+
+    }
+
+    public int getArea(int height, int width) {
+        return 0;
+    }
+
+    public int getArea(int radius){
+        return 0;
+    }
 }
