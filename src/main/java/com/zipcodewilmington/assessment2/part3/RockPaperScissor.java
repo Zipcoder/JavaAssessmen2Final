0972cf5216d64639faaabec016868bbf4e4a4e7c
Owen Murphy <owenmurphy@zipcoders-MacBook-Pro-7.local>
2018-03-05 17:33:11 -0500

    merged

--- src/main/java/com/zipcodewilmington/assessment2/part3/RockPaperScissor.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RockPaperScissor.java
@@ -3,16 +3,5 @@ package com.zipcodewilmington.assessment2.part3;
 public enum RockPaperScissor {ROCK,PAPER_,SCISSOR};
 
 
-        if(ROCK == )
-
-
-
-
-   public RockPaperScissor getWinning{
-
-}
-
-    public RockPaperScissor getLosing
-
 
 
