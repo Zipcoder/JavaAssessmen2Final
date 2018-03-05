b753c7d32c2888edeb4591630b1319f4296ab6a4
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-03-05 16:19:47 -0500

    all tests passing for part7

--- src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
+++ src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
@@ -10,18 +10,25 @@ public class FindTheWinner {
 
 
     public String winner(Integer[]firstArray,Integer[]secondArray,String input){
-
+        int leonScore=0;
+        int wilhemScore =0;
         StringBuilder winner= new StringBuilder();
         if(input.equalsIgnoreCase("odd")){
             if(firstArray[0]%2!=0){
+                for(int i=1;i<firstArray.length;i++){
+                    leonScore+=firstArray[i]-secondArray[i];
+                    wilhemScore+=secondArray[i]-firstArray[i];
+                }
 
-                winner.append(firstArray.getClass().getSimpleName());
+                if(leonScore>wilhemScore){
+                    winner.append("Leon");
+                }else winner.append("Wilhelm");
+
             }
-            else winner.append(secondArray);
         }else if(input.equalsIgnoreCase("Even")){
             if(firstArray[0]%2==0){
-                winner.append(firstArray);
-            }else winner.append(secondArray);
+                winner.append("Leon");
+            }else winner.append("Wilhelm");
         }
         return winner.toString();
     }
