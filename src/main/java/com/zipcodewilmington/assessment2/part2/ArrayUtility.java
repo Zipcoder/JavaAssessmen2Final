6e04935bfac366552a476ad8cf9911a340cafb99
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-03-05 16:26:30 -0500

    resolved

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -1,4 +1,4 @@
-/*package com.zipcodewilmington.assessment2.part2;
+package com.zipcodewilmington.assessment2.part2;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,14 +15,26 @@ public class ArrayUtility<E> {
 
         Collections.addAll(list, theObects);
     }
-}
+
 
 //ArrayUtility<Integer> arrayUtility = new ArrayUtility<>(inputArray);
-/*
-    public E countDuplicatesInMerge(E[] arrayToMerge, Integer valueToEvaluate){
-        for(int i = 0; i <  arrayToMerge.length; i++){
 
-        Collections.addAll(list, arrayToMerge);
+    public E getMostCommonFromMerge(E[] arrayToMerge, E valueToEvaluate) {
+        for (int i = 0; i < arrayToMerge.length; i++) {
+
+            Collections.addAll(list, arrayToMerge);
+        }
+        return arrayToMerge[0];
     }
 
-}*/
+    public E[] removeValue(E value) {
+        for (E object : list) {
+            if (object.equals(value)) {
+                list.remove(object);
+            }
+        }
+      // return list.toArray(new E[0]);
+   return null;
+    }
+
+}
