a6f5adbe808e370adb91f198a3d459fb304f6a33
vvmk <vincentmasiello@gmail.com>
2018-03-05 09:59:32 -0500

    Part2 clean, stubbed, includes part1
    Start tracking complete progress on dev to avoid rebasing...again.

---
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -0,0 +1,36 @@
+package com.zipcodewilmington.assessment2.part2;
+
+/**
+ * filename:
+ * project: question1
+ * author: https://github.com/vvmk
+ * date: 3/5/18
+ */
+public class ArrayUtility<T> {
+    private T[] base;
+
+    public ArrayUtility(T[] base) {
+        this.base = base;
+    }
+
+    public int countDuplicatesInMerge(T[] arrayToMerge, T valueToCheck) {
+        return -1;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+
+    public int getNumberOfOccurrences(T objectToCheck) {
+        return -1;
+    }
+
+    public T[] removeValue(T objectToRemove) {
+        return null;
+    }
+
+    private void mergeArrays() {
+        
+    }
+}
+
