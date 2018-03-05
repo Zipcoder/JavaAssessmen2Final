797bd2b065cde28e8769a2ab6265955b1d189d09
Luis Romero <luisromero@zipcoders-MacBook-Pro-47.local>
2018-03-05 09:48:00 -0500

    testSix() is not correct, passed all other tests

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -8,29 +8,38 @@ public class WuTangConcatenator {
         this.input = input;
     }
 
+    /* Corrections to tests */
+    /*  testSix()
+        Integer input = 6;
+        Assert.assertFalse(isWu); // multiple of 3  --> test should be assertTrue
+        Assert.assertTrue(isTang); // multiple of 5 --> test should be assertFalse
+        Assert.assertFalse(isWuTang); // multiple of 15 --> this is correct
+     */
+
+    // multiple of 3
     public boolean isWu() {
         boolean isWu = false;
-        if ( (input % 3 == 0) && (input % 5 == 0) ) {
-            return isWu;
-        } else if (input % 3 == 0) {
+        if (input % 3 == 0) {
             isWu = true;
         }
         return isWu;
     }
 
+    // multiple of 5
     public boolean isTang() {
         boolean isTang = false;
-        if ( (input % 3 == 0) && (input % 5 == 0) ) {
-            return isTang;
-        } else if (input % 5 == 0) {
+        if (input % 5 == 0) {
             isTang = true;
         }
         return isTang;
     }
 
+    // multiple of 3 and 5
     public boolean isWuTang() {
         boolean isWuTang = false;
-
+        if ( (input % 3 == 0) && (input % 5 == 0) ) {
+            isWuTang = true;
+        }
         return isWuTang;
     }
 
