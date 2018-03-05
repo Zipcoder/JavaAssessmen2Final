055ed39823a826eea095e0a31a85334a84780764
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-03-05 12:05:51 -0500

    part 3 done

--- src/main/java/com/zipcodewilmington/assessment2/part3/RockPaperScissors.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RockPaperScissors.java
@@ -1,47 +1,47 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public enum RockPaperScissors implements Evaluator {
-    //(winningMove, loosingMOve)
-    ROCK("Scissors", "Paper"), PAPER("Rock", "Scissors" ), SCISSORS("Paper","Rock");
+public enum RockPaperScissors {
+    PAPER, SCISSORS, ROCK;
 
-    private  String winningMove;
-    private final String loosingMove;
-
-    RockPaperScissors(String winningMove, String loosingMove) {
-        this.winningMove = winningMove;
-        this.loosingMove = loosingMove;
+    public RockPaperScissors getWinningSign() {
+        switch (this) {
+            case ROCK:
+                return PAPER;
+            case PAPER:
+                return SCISSORS;
+            case SCISSORS:
+                return ROCK;
+        }
+        return this;
     }
 
-    public String getWinningSign() {
+    public RockPaperScissors getLosingSign() {
+        switch (this) {
+            case ROCK:
+                return SCISSORS;
+            case PAPER:
+                return ROCK;
+            case SCISSORS:
+                return PAPER;
+        }
 
-        return winningMove;
-    }
-
-    public String getLosingSign() {
-        return loosingMove;
-    }
-
-    public void evaluateWinnerTest(){
-
-    }
-
-
-    public void evaluateLoserTest() {
+        return this;
     }
 }
 
 
-    /**
-     * @param handSignOfPlayer1 a string representative of a hand sign of a player
-     * @param handSignOfPlayer2 a string representative of a hand sign of a challenger
-     * @return a string representative of the winning hand sign between the two players
-
-    public String getWinner(String handSignOfPlayer1, String handSignOfPlayer2) {
-        if(handSignOfPlayer1.equals(handSignOfPlayer2)) {
-            return "Tied!";
-        } else if(RockPaperSissorsEvaluator.getLosingMove(handSignOfPlayer1) == handSignOfPlayer2){
-            return handSignOfPlayer2;
-        } else {
-            return handSignOfPlayer1; }
-    }
-}*/
+/**
+ * @param handSignOfPlayer1 a string representative of a hand sign of a player
+ * @param handSignOfPlayer2 a string representative of a hand sign of a challenger
+ * @return a string representative of the winning hand sign between the two players
+ * <p>
+ * public String getWinner(String handSignOfPlayer1, String handSignOfPlayer2) {
+ * if(handSignOfPlayer1.equals(handSignOfPlayer2)) {
+ * return "Tied!";
+ * } else if(RockPaperSissorsEvaluator.getLosingMove(handSignOfPlayer1) == handSignOfPlayer2){
+ * return handSignOfPlayer2;
+ * } else {
+ * return handSignOfPlayer1; }
+ * }
+ * }
+ */
