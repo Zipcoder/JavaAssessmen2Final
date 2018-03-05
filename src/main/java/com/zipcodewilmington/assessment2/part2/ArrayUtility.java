34a3def7d57a438d8adc2f5cca427e576a0e2713
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-05 15:42:45 -0500

    saving

--- src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -12,29 +12,58 @@ public class ArrayUtility<T> {
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
         T[] mergedArray = Arrays.copyOf(inputArray, inputArray.length);
-        for(int i = 0; i < arrayToMerge.length; i++){
+        for(int i = inputArray.length; i < arrayToMerge.length + inputArray.length; i++){
             mergedArray = Arrays.copyOf(mergedArray, mergedArray.length+1);
-            mergedArray[i] = arrayToMerge[i];
+            mergedArray[i] = arrayToMerge[i-inputArray.length];
         }
         Integer outputCounter = 0;
-        for(T t : mergedArray){
-            if(t == valueToEvaluate){
+
+        for(int j = 0; j< mergedArray.length; j++){
+            if(mergedArray[j] == valueToEvaluate){
                 outputCounter++;
             }
         }
         return outputCounter;
     }
 
-    public T getMostCommonFromMerge(T[] arrayToMerge, T[] inputArray ){
-        return null;
+    public T getMostCommonFromMerge(T[] arrayToMerge){
+        T[] mergedArray = Arrays.copyOf(inputArray, inputArray.length);
+        for(int i = inputArray.length; i < arrayToMerge.length + inputArray.length; i++){
+            mergedArray = Arrays.copyOf(mergedArray, mergedArray.length+1);
+            mergedArray[i] = arrayToMerge[i-inputArray.length];
+        }
+        this.inputArray = mergedArray;
+        T mostCommon = mergedArray[0];
+        int highestOccurrence = 0;
+        for(T t : inputArray){
+            if(this.getNumberOfOccurrences(t) > highestOccurrence){
+                highestOccurrence = this.getNumberOfOccurrences(t);
+                mostCommon = t;
+            }
+        }
+        return mostCommon;
     }
 
     public Integer getNumberOfOccurrences(T valueToCount){
-        return 0;
+        int counter = 0;
+        for(T t : this.inputArray){
+            if (t.equals(valueToCount)){
+                counter++;
+            }
+        }
+
+        return counter;
     }
 
     public T[] removeValue(T valueToRemove){
-        return null;
+        T[] outputArray = Arrays.copyOf(this.inputArray, 0);
+        for(int i = 0; i<inputArray.length; i++){
+            if(!inputArray[i].equals(valueToRemove)){
+                outputArray = Arrays.copyOf(outputArray, outputArray.length+1);
+                outputArray[outputArray.length-1] = inputArray[i];
+            }
+        }
+        return outputArray;
     }
 
 }
