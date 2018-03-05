0143f383c1970663edd303a352bed58784861ac5
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-05 15:15:08 -0500

    class set up

---
+++ src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
@@ -0,0 +1,15 @@
+package com.zipcodewilmington.assessment2.part7;
+
+public class FindTheWinner {
+
+    private String winner = "";
+
+    public FindTheWinner(){
+
+    }
+    public String winner(Integer[] leon, Integer[] wilhelm, String input) {
+
+        return winner;
+
+    }
+}
