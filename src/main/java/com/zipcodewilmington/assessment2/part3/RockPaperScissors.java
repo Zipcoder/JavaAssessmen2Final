a5c0580e4ea3b86517f97de6b997c3637b8caf6d
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-03-05 11:34:22 -0500

    needs work

---
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RockPaperScissors.java
@@ -0,0 +1,73 @@
+package com.zipcodewilmington.assessment2.part3;
+
+public enum RockPaperScissors {
+
+    ROCK,
+    PAPER,
+    SCISSORS;
+
+
+    RockPaperScissors input;
+
+
+    RockPaperScissors() {
+
+    }
+
+
+    public RockPaperScissors getWinningSign() {
+
+        RockPaperScissors input = RockPaperScissors.ROCK;
+
+        switch (input) {
+
+            case ROCK:
+                return PAPER;
+
+            case SCISSORS:
+                return ROCK;
+
+            case PAPER:
+                return SCISSORS;
+
+            default:
+                break;
+
+
+//        if (input.equals(ROCK))
+//            return RockPaperScissors.PAPER;
+//        else if (input.equals(PAPER))
+//            return RockPaperScissors.SCISSORS;
+//         else if (input.equals(SCISSORS))
+//            return RockPaperScissors.ROCK;
+//
+//
+//        return null;
+        }
+        return ROCK;
+    }
+
+    public RockPaperScissors getLosingSign() {
+
+        RockPaperScissors input = RockPaperScissors.PAPER;
+
+        switch (input) {
+
+            case ROCK:
+                return SCISSORS;
+
+
+            case SCISSORS:
+                return PAPER;
+
+            case PAPER:
+                return ROCK;
+
+            default:
+                break;
+
+        }
+        return null;
+    }
+}
+
