c9d07b5a4826451541719e84577602ad1e0ed247
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-03-05 17:24:54 -0500

    fix part2

---
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -0,0 +1,12 @@
+package com.zipcodewilmington.assessment2.part2;
+
+public class ArrayUtility <T>{
+
+    public ArrayUtility(T[] inputArray){
+
+    }
+
+    public Integer countDuplicatesInMerge(T[] valueToEvaluate, T value){
+        return null;
+    }
+}
