e03b01f30d699a166df4f38bccf13b6cb8c649d3
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-03-05 13:48:52 -0500

    three out of four passed

---
+++ src/main/java/com/zipcodewilmington/assessment2/part5/ElectionLand.java
@@ -0,0 +1,40 @@
+package com.zipcodewilmington.assessment2.part5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ElectionLand {
+
+
+    public ElectionLand() {
+    }
+
+    public static String getMostCommon(String[] votes) {
+        ArrayList<String> commonVotes = new ArrayList<>();
+
+
+        for (int i = 1; i < votes.length; i++){
+            for (int j = 0; j < (votes.length - 1); j++){
+                if (!votes[i].equals(votes[j])){
+                    commonVotes.remove(votes[j]);
+                }
+                if (votes[i].equals(votes[j]))
+                    commonVotes.add(votes[j]); {
+                }
+
+            }
+        }
+        Collections.sort(commonVotes);
+        String[] cvArray = commonVotes.toArray(new String[commonVotes.size()]);
+
+        return cvArray[cvArray.length - 1];
+    }
+
+
+    public String electionWinner(String[] votes) {
+
+            return getMostCommon(votes);
+    }
+
+
+}
