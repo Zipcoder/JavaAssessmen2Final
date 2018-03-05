82b2d2d451a9cb68c91f4b48e5894a1843834ca8
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-03-05 16:04:44 -0500

    mostly finished part 5, one test failed, not sure why

--- src/main/java/com/zipcodewilmington/assessment2/part5/ElectionLand.java
+++ src/main/java/com/zipcodewilmington/assessment2/part5/ElectionLand.java
@@ -5,23 +5,52 @@ import java.util.*;
 public class ElectionLand {
     String[] votes;
 
+
     public String electionWinner(String[] votes) {
+        ArrayList candidates = new ArrayList<String>(Arrays.asList(votes));
+        Collections.sort(candidates);
 
-        int votePerPerson = 0;
-        List asList = Arrays.asList(votes);
-        Set<String> mySet = new HashSet<String>(asList);
-        for(String s: mySet){
-
-            Collections.frequency(asList,s);
-
-
+        int totalSize = candidates.size();
+        int count = 0;
+        String winner = null;
+        for (String current : votes) {
+            int running = 0;
+            for (String other : votes) {
+                if (current.equals(other)) {
+                    running++;
+                }
+            }
+            if (running >= count) {
+                count = running;
+                winner = current;
+            }
         }
-//        for (int i = 0; i < votes.length; i++) {
-//            if (votes[i].equalsIgnoreCase(votes[i])){
-//
-//
-//            }
-//        }
-    return null;
+        return winner;
     }
 }
+
+
+// public String electionWinner(String[] votes) {
+////        ArrayList countedVotes = new ArrayList<String>(Arrays.asList(votes));
+////
+////        int winnerCount = 0;
+////        String winner = null;
+////
+////        for (int i = 0; i < countedVotes.size(); i++) {
+////            String currentPerson = votes[i];
+////            int currentVotes = 0;
+////            for (int j = 0; j < countedVotes.size(); j++) {
+////                if (currentPerson == votes[j]) {
+////                    currentVotes++;
+////                    winnerCount += currentVotes;
+////                    currentPerson = winner;
+////                    if (currentVotes >= winnerCount) {
+////                        currentPerson = winner;
+////                    }
+////                }
+////
+////            }
+////            return winner;
+////
+////        }return null;
+////    }
