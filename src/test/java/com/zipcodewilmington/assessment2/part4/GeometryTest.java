1a1dc9f28db4cae0e289053ddb527acc923605d3
vvmk <vincentmasiello@gmail.com>
2018-03-05 12:02:54 -0500

    Fix part4 impl test, convert sort to lambda

--- src/test/java/com/zipcodewilmington/assessment2/part4/GeometryTest.java
+++ src/test/java/com/zipcodewilmington/assessment2/part4/GeometryTest.java
@@ -17,7 +17,7 @@ public class GeometryTest {
         Geometry g = new Geometry(height,width,radius);
         int expected = 10;
         int actual = g.getArea(height, width);
-        Assert.assertEquals("The rectangle area should be 10", expected, actual);
+        Assert.assertEquals("The rectangle getArea should be 10", expected, actual);
     }
 
     @Test
@@ -28,7 +28,7 @@ public class GeometryTest {
         Geometry g = new Geometry(height,width,radius);
         double expected = 153.86;
         double actual = g.getArea(radius);
-        Assert.assertEquals("The Circle area should be 153.86", expected, actual, 0.001);
+        Assert.assertEquals("The Circle getArea should be 153.86", expected, actual, 0.001);
     }
 
     @Test
@@ -39,7 +39,7 @@ public class GeometryTest {
         Geometry g = new Geometry(height,width,radius);
         int expected = 32;
         int actual = g.getArea(height, width);
-        Assert.assertEquals("The rectangle area should be 32", expected, actual);
+        Assert.assertEquals("The rectangle getArea should be 32", expected, actual);
     }
 
     @Test
@@ -50,7 +50,7 @@ public class GeometryTest {
         Geometry g = new Geometry(height,width,radius);
         double expected = 1384.74;
         double actual = g.getArea(radius);
-        Assert.assertEquals("The Circle area should be 1384.74", expected, actual, 0.001);
+        Assert.assertEquals("The Circle getArea should be 1384.74", expected, actual, 0.001);
     }
 
     @Test
@@ -65,18 +65,13 @@ public class GeometryTest {
         Class[] names = sortInterfaceArrayAlphabetically(o.getClass().getInterfaces());
         String[] namesAsString = new String[names.length];
         for (int x=0; x < names.length; x++){
-            namesAsString[x] = names[x].getName();
+            namesAsString[x] = names[x].getSimpleName();
         }
         return namesAsString;
     }
 
     private Class[] sortInterfaceArrayAlphabetically(Class[] arr){
-        Arrays.sort(arr, new Comparator<Class>() {
-            @Override
-            public int compare(Class o1, Class o2) {
-                return o1.getName().compareTo(o2.getName());
-            }
-        });
+        Arrays.sort(arr, (o1, o2) -> o1.getName().compareTo(o2.getSimpleName()));
         return arr;
     }
 
