e812ae9790cfd4bd4d0d26b98a3bbf3a1f6c2670
ahsonali <ahsonac@gmail.com>
2018-03-05 10:49:07 -0500

    test commit

---
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -0,0 +1,18 @@
+package com.zipcodewilmington.assessment2.part2;
+
+public class ArrayUtility
+{
+    //We'll need a private arraylist
+    //Integer count of 0
+
+
+    //got through entire arrayList
+
+
+
+    //count each object in the arraylist
+
+
+
+
+}
---
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RockPaperScissor.java
@@ -0,0 +1,38 @@
+package com.zipcodewilmington.assessment2.part3;
+
+public enum RockPaperScissor
+{
+    ROCK("ROCK"),
+    SCISSORS("SCISSOR"),
+    PAPER("PAPER");
+
+    String winningMove;
+
+    RockPaperScissor(String winningMove)
+    {
+        this.winningMove = winningMove;
+    }
+
+
+    //getWinningSign method
+
+    //getLosingSign method
+
+
+//    private String winner;
+//
+//    RockPaperScissor(String winner)
+//    {
+//        this.winner = winner;
+//    }
+//
+//    public String getWinner()
+//    {
+//        return this.winner;
+//    }
+
+
+
+
+
+}
