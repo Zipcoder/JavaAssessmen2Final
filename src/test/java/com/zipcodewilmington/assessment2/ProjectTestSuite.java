de891f9ee7b26132dd7a42609a25c573344eff91
Kaitrina High <kaitrinahigh@zipcoders-MacBook-Pro-81.local>
2018-03-05 13:18:00 -0500

    tests passing, had to change projecttestsuite file for correct path

--- src/test/java/com/zipcodewilmington/assessment2/ProjectTestSuite.java
+++ src/test/java/com/zipcodewilmington/assessment2/ProjectTestSuite.java
@@ -1,7 +1,7 @@
 package com.zipcodewilmington.assessment2;
 
-import com.zipcodewilmington.assessment2.part1.WuTangConcatenatorTest;
-import com.zipcodewilmington.assessment2.part2.ArrayUtilityTestSuite;
+import com.zipcodewilmington.assessment2.part5.ElectionLandTest;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -12,8 +12,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses({
-        ArrayUtilityTestSuite.class,
-        WuTangConcatenatorTest.class,
+        ElectionLandTest.class,
+
 })
 public class ProjectTestSuite {
 }
