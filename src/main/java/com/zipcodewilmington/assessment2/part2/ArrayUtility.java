2b315fb5f8203dc0e7b5c5d94294d556596ecf76
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-05 09:43:13 -0500

    saving

---
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ArrayUtility.java
@@ -0,0 +1,27 @@
+package com.zipcodewilmington.assessment2.part2;
+
+public class ArrayUtility<T> {
+
+    T[] inputArray;
+
+    public ArrayUtility(T[] input){
+        this.inputArray = input;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate){
+        return 0;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge, T expected ){
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToCount){
+        return 0;
+    }
+
+    public T[] removeValue(T valueToRemove){
+        return null;
+    }
+
+}
