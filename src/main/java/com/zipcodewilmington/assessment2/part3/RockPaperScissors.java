53c930710951b7044a3282e0786e9a197a8edfa9
Garrett Arant <garrettarant@MacBook-Pro-4.local>
2018-03-05 13:44:39 -0500

    Completed Part 3

---
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RockPaperScissors.java
@@ -0,0 +1,33 @@
+package com.zipcodewilmington.assessment2.part3;
+
+public enum RockPaperScissors {
+
+    ROCK,
+    SCISSORS,
+    PAPER;
+
+
+    public RockPaperScissors getWinningSign() {
+
+        if (this.equals(PAPER)) {
+            return SCISSORS;
+        } else if (this.equals(ROCK)) {
+            return PAPER;
+        } else if (this.equals(SCISSORS)) {
+            return ROCK;
+        }
+        return null;
+
+    }
+
+    public RockPaperScissors getLosingSign() {
+        if (this.equals(PAPER)) {
+            return ROCK;
+        } else if (this.equals(ROCK)) {
+            return SCISSORS;
+        } else if (this.equals(SCISSORS)) {
+            return PAPER;
+        }
+        return null;
+    }
+}
