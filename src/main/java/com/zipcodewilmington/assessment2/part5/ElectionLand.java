d6473053def89c97119030f0e238f0571aa11368
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-05 13:03:37 -0500

    mysteryland

---
+++ src/main/java/com/zipcodewilmington/assessment2/part5/ElectionLand.java
@@ -0,0 +1,16 @@
+package com.zipcodewilmington.assessment2.part5;
+
+public class ElectionLand {
+
+    private String votes = "";
+    private String personWhoGotElected = "";
+
+    public ElectionLand(){
+
+    }
+
+
+    public String electionWinner(String[] votes) {
+        return personWhoGotElected;
+    }
+}
