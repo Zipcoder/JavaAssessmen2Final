496ff71b0dddfd9c0ae3a451102dab15a0238730
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-13 16:14:06 -0400

    updated geomtry et al

--- src/main/java/com/zipcodewilmington/assessment2/part3/RockPaperScissors.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/RockPaperScissors.java
@@ -35,3 +35,66 @@ public enum RockPaperScissors {
 
     }
 }
+
+/*  NOTE: all you technically needed for this was your RockPaperScissors class.
+
+
+
+option 1:
+switch way of doing it :
+
+public enum RockPaperScissors {
+    PAPER,
+    ROCK,
+    SCISSORS;
+
+    public RockPaperScissors getWinner() {
+        switch (this) {
+            case SCISSORS:
+                return ROCK;
+            case ROCK:
+                return PAPER;
+            default:
+                return SCISSORS;
+        }
+    }
+
+    public RockPaperScissors getLoser() {
+        return getWinner().getWinner();
+    }
+}
+
+
+option2:
+
+
+
+leon's enum solution from slack post test:
+
+public enum RockPaperScissors1 {
+    PAPER(null),
+    ROCK(PAPER),
+    SCISSORS(ROCK);
+
+    private final RockPaperScissors1 winner;
+
+    RockPaperScissors1(RockPaperScissors1 winner) {
+        this.winner = winner;
+    }
+
+    public RockPaperScissors1 getWinner() {
+        if(winner == null) {
+            return SCISSORS;
+        }
+        return winner;
+    }
+
+    public RockPaperScissors1 getLoser() {
+        return getWinner().getWinner();
+    }
+}
+
+ */
+
+
+
--- src/main/java/com/zipcodewilmington/assessment2/part4/Geometry.java
+++ src/main/java/com/zipcodewilmington/assessment2/part4/Geometry.java
@@ -1,6 +1,6 @@
 package com.zipcodewilmington.assessment2.part4;
 
-public class Geometry {
+public class Geometry implements Rectangle, Circle {
     private int height;
     private int width;
     private int radius;
@@ -13,15 +13,30 @@ public class Geometry {
     }
 
     public Geometry(int height, int width, int radius){
+        this.height = height;
+        this. width = width;
+        this.radius = radius;
 
 
     }
 
     public int getArea(int height, int width) {
-        return height * width;
+        //return height * width;
+        return area(height,width);
     }
 
     public double getArea(int radius){
+        //return pi*radius*radius;
+        return area(radius);
+    }
+
+    @Override
+    public double area(int radius) {
         return pi*radius*radius;
+    }
+
+    @Override
+    public int area(int height, int width) {
+        return height * width;
     }
 }
