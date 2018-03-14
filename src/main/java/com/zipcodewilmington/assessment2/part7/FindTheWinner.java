586ccedd652e332a4ebf6479253f6ded6179ea5a
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-14 15:10:18 -0400

    Find the winner now works yay

--- src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
+++ src/main/java/com/zipcodewilmington/assessment2/part7/FindTheWinner.java
@@ -2,6 +2,33 @@ package com.zipcodewilmington.assessment2.part7;
 
 public class FindTheWinner {
 
+    public String winner(Integer[] leon, Integer[] wilhem, String input) {
+
+        Integer player1Score = 0;
+        Integer player2Score = 0;
+
+//        int i = (input.equalsIgnoreCase("Even") ? 0:1);
+
+        for (int i = (input.equalsIgnoreCase("Even") ? 0:1); i <leon.length; i+=2){
+            player1Score += leon[i]-wilhem[i];
+            player2Score += wilhem[i]-leon[i];
+
+        }
+
+       String winner = player1Score > player2Score ? "Leon": "Wilhelm";
+
+
+        return winner;
+
+    }
+}
+
+/*here is what amy ended up with on test day:
+
+package com.zipcodewilmington.assessment2.part7;
+
+public class FindTheWinner {
+
     private String winner = "";
 
     public FindTheWinner(){
@@ -23,3 +50,9 @@ public class FindTheWinner {
 
     }
 }
+
+ */
+
+/*
+
+ */
